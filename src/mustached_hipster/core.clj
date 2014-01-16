f21e573947bc1c9cce22df7b0302183dda3a24a0
Paul DeLong <pd72@nyu.edu>
2014-01-16 14:10:55 -0500

    cruft removal;

--- src/mustached_hipster/core.clj
+++ src/mustached_hipster/core.clj
@@ -4,9 +4,7 @@
 (require 'clojure.string)
 
 (defn -main
-  [url]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  ;(print (:body (clj-http.client/get "http://nrk.no/nordlandsbanen/")))
-  ;(print (:body (clj-http.client/get "http://nrk.no/nordlandsbanen/media/js/videoPlayer.js")))
-  (print (:body (clj-http.client/get url))))
+   [url]
+   ;; work around dangerous default behaviour in Clojure
+   (alter-var-root #'*read-eval* (constantly false))
+   (print (:body (clj-http.client/get url))))

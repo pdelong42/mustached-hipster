bde1534274cd56d3f26d0ed0364e8292730c8623
Paul DeLong <pd72@nyu.edu>
2014-01-16 16:04:19 -0500

    parse out the URLs in the playlist;

--- src/mustached_hipster/core.clj
+++ src/mustached_hipster/core.clj
@@ -1,10 +1,18 @@
-(ns mustached-hipster.core (:gen-class))
-
-(require 'clj-http.client)
-(require 'clojure.string)
+(ns mustached-hipster.core
+   (:require clj-http.client clojure.string)
+   (:gen-class))
 
 (defn -main
    [url]
+
    ;; work around dangerous default behaviour in Clojure
    (alter-var-root #'*read-eval* (constantly false))
-   (print (:body (clj-http.client/get url))))
+
+   (print
+      (clojure.string/join
+         "\n"
+         (map
+            #(first (clojure.string/split (clojure.string/trim %) #"\?"))
+            (filter
+               #(not= \# (first %))
+               (clojure.string/split-lines (:body (clj-http.client/get url))))))))

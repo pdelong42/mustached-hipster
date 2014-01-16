118c1b2c95699356426ab8374bbdfa0a5c7ba324
Paul DeLong <pd72@nyu.edu>
2014-01-16 14:09:56 -0500

    showed previous URLs grabbed in the "breadcrumb trail" used to get here;

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-lein run http://nrk_minutt_od_l-vh.akamaihd.net/i/program/program_,141,316,563,1266,2250,.mp4.csmil/index_4_av.m3u8 > foo.txt
+#!/bin/sh
+
+#lein run http://nrk.no/nordlandsbanen/
+#lein run http://nrk.no/nordlandsbanen/media/js/videoPlayer.js
+lein run http://nrk_minutt_od_l-vh.akamaihd.net/i/program/program_,141,316,563,1266,2250,.mp4.csmil/index_4_av.m3u8

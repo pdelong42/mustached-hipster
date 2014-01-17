4800dfc07e50b96a069764b0574337d3e19656c2
Paul DeLong <pd72@nyu.edu>
2014-01-17 11:06:47 -0500

    prep for standalone run without leiningen;

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -x
 
-#lein run http://nrk.no/nordlandsbanen/
-#lein run http://nrk.no/nordlandsbanen/media/js/videoPlayer.js
-lein run http://nrk_minutt_od_l-vh.akamaihd.net/i/program/program_,141,316,563,1266,2250,.mp4.csmil/index_4_av.m3u8
+#URL=http://nrk.no/nordlandsbanen/
+#URL=http://nrk.no/nordlandsbanen/media/js/videoPlayer.js
+URL=http://nrk_minutt_od_l-vh.akamaihd.net/i/program/program_,141,316,563,1266,2250,.mp4.csmil/index_4_av.m3u8
+
+lein run $URL
+#java -jar target/mustached-hipster-0.1.0-SNAPSHOT-standalone.jar $URL

4b87e337bdf16c3a7cdd6ca916b2528e4e617fc6
Paul DeLong <Paul.DeLong@gmail.com>
2014-01-17 23:23:51 -0500

    moved HTTP GET and supporting logic into its own function;

--- src/mustached_hipster/core.clj
+++ src/mustached_hipster/core.clj
@@ -3,21 +3,27 @@
    (:require [clj-http.client :as http :only [get]])
    (:gen-class))
 
+(defn get-playlist
+   [m3u8]
+
+   (let
+      [before (System/nanoTime)]
+
+      (let
+         [response (http/get m3u8)]
+         (println "the request took" (* (- (System/nanoTime) before) 1e-9) "seconds")
+         response)))
+
 (defn -main
    [url]
 
    ;; work around dangerous default behaviour in Clojure
    (alter-var-root #'*read-eval* (constantly false))
 
-   (let
-      [before (System/nanoTime)]
-
-      (spit "foo.txt"
-         (apply str (take 10
-            (map
-               #(s/replace % #"\?.*" "\n")
-               (filter
-                  #(not= \# (first %))
-                  (s/split-lines (:body (http/get url))))))))
-
-      (println "the whole thing took" (* (- (System/nanoTime) before) 1e-9) "seconds")))
+   (spit "foo.txt"
+      (apply str (take 10
+         (map
+            #(s/replace % #"\?.*" "\n")
+            (filter
+               #(not= \# (first %))
+               (s/split-lines (:body (get-playlist url)))))))))
